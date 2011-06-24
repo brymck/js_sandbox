5be9b4695f2e7de94d9a31f141358b80b2aa5dd8
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-06-24 13:37:22 +0900

    Cleaned up sandbox code

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,4 +1,27 @@
+/**
+ * @preserve js_sandbox v0.5
+ * http://www.brymck.com
+ *
+ * Copyright 2011, Bryan McKelvey
+ * Licensed under the MIT License
+ * http://www.brymck.com/license
+ */
+
+/**
+ * A class handling code loading, saving and running in the sandbox
+ */
 var Sandbox = (function() {
+  /**
+   * Invalid names for local storage.
+   * @type {Array.<string>}
+   * @const
+   */
+  var RESERVED_NAMES = ["", "length"];
+
+  /**
+   * Enum for key codes.
+   * @enum {number}
+   */
   var KeyCodes = {
     D: 68,
     R: 82,
@@ -13,15 +36,37 @@ var Sandbox = (function() {
   var saved = document.getElementById("saved");
   var saveBtn = document.getElementById("save_btn");
   var delBtn = document.getElementById("del_btn");
+  var self;
 
+  /**
+   * Adds event listeners to the sandbox DOM.
+   */
   function addListeners() {
     form.onsubmit = Sandbox.run;
     form.onkeydown = keyHandler;
     saved.onchange = loadSave;
-    saveBtn = Sandbox.save;
-    delBtn = Sandbox.remove;
+    saveBtn.onclick = Sandbox.save;
+    delBtn.onclick = Sandbox.remove;
   }
 
+  /**
+   * Checks whether the provided name matches a reserved name for storage.
+   * @param {?string} name The name to check.
+   * @return {boolean} Whether the name is reserved.
+   */
+  function matchReservedName(name) {
+    for (var i = 0; i < RESERVED_NAMES.length; i++) {
+      if (name === RESERVED_NAMES[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Handles key presses.
+   * @param {Event} e A key press event.
+   */
   function keyHandler(e) {
     if (e.ctrlKey && e.shiftKey) {
       switch (e.which) {
@@ -44,15 +89,16 @@ var Sandbox = (function() {
     keycode.value = e.which;
   }
 
-  function loadSave(e) {
-    Sandbox.load(e.target.value);
-  }
-
+  /**
+   * Lists the names of saved code snippets in a dropdown.
+   * @param {string=} loaded The name of the option to automatically select
+   *   (optional).
+   */
   function listSaves(loaded) {
     var keys = [];
-    if (!("" in localStorage)) {
-      localStorage[""] = "";
-    }
+
+    // Always keep a blank
+    localStorage[""] = "";
 
     // Clear existing options
     while (saved.firstChild) {
@@ -74,25 +120,53 @@ var Sandbox = (function() {
     }
   }
 
+  /**
+   * Handles an event on a select object by loading the requested code.
+   * @param {Event} e An event on a select object.
+   */
+  function loadSave(e) {
+    self.load(e.target.value);
+  }
+
   return {
+    /**
+     * Initializes the sandbox code by listing saves in the dropdown boxes and
+     * adding listeners for button clicks and key presses.
+     */
     init: function() {
+      self = this;
       listSaves();
       addListeners();
     },
 
+    /**
+     * Loads a code snippet based on the provided name.
+     * @param {?string} name The name of the code snippet to load.
+     */
     load: function(name) {
       code.value = (name === "" ? "" : JSON.parse(localStorage[name]));
     },
 
+    /**
+     * Removes a saved code snippet.
+     * @param {?string=} name The name of the saved code to remove.
+     */
     remove: function(name) {
-      name = name || saved.value;
-      if (confirm("Are you sure you wish to delete '" + name + "'?")) {
-        code.value = "";
-        delete localStorage[name];
-        listSaves();
+      if (typeof name !== "string") {
+        name = saved.value;
+      }
+      if (name !== "") {
+        if (confirm("Are you sure you wish to delete '" + name + "'?")) {
+          code.value = "";
+          delete localStorage[name];
+          listSaves();
+        }
       }
     },
 
+    /**
+     * Runs code and returns the time elapsed and any error messages.
+     */
     run: function() {
       try {
         var count = parseInt(iter.value, 10),
@@ -112,10 +186,18 @@ var Sandbox = (function() {
       }
     },
 
+    /**
+     * Saves code to local storage.
+     */
     save: function() {
-      var name = prompt("Please enter a name for this code:");
-      localStorage[name] = JSON.stringify(code.value);
-      listSaves(name);
+      var name;
+      do {
+        name = prompt("Please enter a name for this code:");
+      } while (matchReservedName(name));
+      if (name !== null) {
+        localStorage[name] = JSON.stringify(code.value);
+        listSaves(name);
+      }
     }
   }
 })();
